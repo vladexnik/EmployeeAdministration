34f80ec7c277ecd0abb4ec79861eb2b2c0bf8b1e
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-03-22 15:22:01 +0300

    create react components

---
+++ src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,15 @@
+
+
+const EmployeesListItem = () => {
+    return (
+        <li classname="employees-list-item">
+            <span className="nameEmployee"> Vatson Bert</span>
+            <div className="threeButtons">
+                <button type="button" className='delete'>
+
+                </button>
+            </div>
+        </li>
+    )
+}   
+
